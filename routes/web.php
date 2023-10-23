345564bc21a49eef968f4f82dda4ccc85c036832
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-10-23 21:33:01 +0900

    web.phpにURLをつくってみた

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/calcs/{value1}/{operator}/{value2}', function ($value1, $operator, $value2) {
+    return view('message.calc', 
+    [
+        'value1' => $value1, 
+        'operator' => $operator, 
+        'value2' => $value2
+    ]);
+});
